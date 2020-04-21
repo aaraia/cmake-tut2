fb00315d330117d4cd7a5fb97e9483ec73dd83ba
creativexp <contact@superrockgames.com>
2020-04-21 16:56:09 +0100

    relative location for file directory printout test

--- proj/src/main.cpp
+++ proj/src/main.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    for (boost::filesystem::directory_entry& e : boost::filesystem::directory_iterator("C:\\Boost\\boost_1_72_0"))
+    for (boost::filesystem::directory_entry& e : boost::filesystem::directory_iterator("../"))
         std::cout << e.path() << '\n';
 
     std::cout << "Hello, World " << " lib(5) = " << lib::number(5) << " box(5) = " << box::number(5);
